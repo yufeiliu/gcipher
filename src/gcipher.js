33f9152c87c3e433110cdb1f05aa7e7f29045ab6
Yufei Liu <liuyufei@gmail.com>
2012-04-03 16:36:04 -0400

    created web page for instruction, refactored library code

---
+++ src/gcipher.js
@@ -0,0 +1,39 @@
+/*
+	gcipher by @yufeiliu
+	(c) Yufei Liu
+	Licensed under GPLv3
+*/
+
+(function(){
+	function gcipher() {
+		var sig = "{Encrypted with gcipher}";
+		var pw = prompt("What is the password?");
+		var c = new Blowfish(pw);
+		var doc = document.querySelector("#canvas_frame").contentWindow.document;
+		var msgFrame = doc.querySelectorAll("iframe")[1];
+		if (typeof(msgFrame)==="undefined") {
+			var msgContainers = doc.querySelectorAll("div.ii");
+			for (var i = 0; i < msgContainers.length; i++) {
+				var cur = msgContainers[i].children[0];
+				if (typeof(cur)!=="undefined" && cur.innerHTML.substr(0,sig.length)===sig) {
+					cur.innerHTML = c.decrypt(cur.innerHTML.substr(sig.length).replace(/<wbr>/g,"").replace(/\n/g,""));
+				}
+			}
+		} else {
+			var msgBody = msgFrame.contentWindow.document.querySelector("body.editable");
+			msgBody.innerHTML = sig+c.encrypt(msgBody.innerHTML);
+		}
+	}
+
+	//Load required library
+	if (document.getElementById("blowfish-js")===null) {
+		s=document.createElement("script");
+		s.setAttribute("src","https://raw.github.com/yufeiliu/blowfish.js/master/blowfish.js");
+		s.setAttribute("id","blowfish-js");
+		s.onload = gcipher;
+		document.body.appendChild(s);
+	} else {
+		gcipher();
+	}
+
+})();
